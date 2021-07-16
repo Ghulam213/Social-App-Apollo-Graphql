1e78439f1c756684dd7f77cb14c588972c292eb7
Ghulam213 <gmuhammad213@gmail.com>
2021-07-16 23:26:30 +0500

    completed

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    'mongodb+srv://Ghulam2user:ghulam2user@cluster0.4mdc1.mongodb.net/SocialMediaApp?retryWrites=true&w=majority',
-  JWT_SECRET: 'somethingVerySecret',
-}
